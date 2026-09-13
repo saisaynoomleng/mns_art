cd6ba9bfd9c1296b89f1596da7142b5b1af7f2c9
saisaynoomleng <saileng9723@gmail.com>
2026-09-13 13:58:08 -0400

    ui_bounded

--- packages/ui/src/components/shared/Bounded/Bounded.tsx
+++ packages/ui/src/components/shared/Bounded/Bounded.tsx
@@ -1,3 +1,56 @@
-export const Bounded = () => {
-  return <div>asdfasdf</div>;
+import { clsx, twMerge } from 'cn';
+import React, { ComponentPropsWithoutRef } from 'react';
+
+type BoundedProp<T extends React.ElementType> = {
+  className?: string;
+  as?: T;
+  padding?: Padding;
+  spacing?: Spacing;
+  children: React.ReactNode;
+} & Omit<ComponentPropsWithoutRef<T>, 'classname' | 'as'>;
+
+type Padding = 'none' | 'sm' | 'md' | 'lg';
+
+type Spacing = 'none' | 'sm' | 'md' | 'lg';
+
+const paddingVariants: Record<Padding, string> = {
+  none: '',
+  sm: 'px-4 md:px-6 lg:px-8',
+  md: 'px-6 md:px-8 lg:px-10',
+  lg: 'px-8 md:px-10 lg:px-12',
+};
+
+const spacingVariants: Record<Spacing, string> = {
+  none: '',
+  sm: 'space-y-6 md:space-y-8 lg:space-y-10',
+  md: 'space-y-8 md:space-y-10 lg:space-y-12',
+  lg: 'space-y-10 md:space-y-12 lg:space-y-16',
+};
+
+export const Bounded = <T extends React.ElementType>({
+  className,
+  as,
+  padding = 'sm',
+  children,
+  spacing = 'none',
+  ...props
+}: BoundedProp<T>) => {
+  const Comp = as ?? 'section';
+
+  return (
+    <Comp
+      data-testid="wrapper"
+      className={twMerge(
+        clsx(
+          'py-4 md:py-6 lg:py-10',
+          paddingVariants[padding],
+          spacingVariants[spacing],
+          className,
+        ),
+      )}
+      {...props}
+    >
+      {children}
+    </Comp>
+  );
 };
